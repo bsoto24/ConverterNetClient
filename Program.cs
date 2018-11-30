d5d35134eb0820f9fd80b2095e5b051d4f10e19b
pepitofriki <bsoto.dev@gmail.com>
2018-11-30 15:58:04 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using Newtonsoft.Json;
+
+namespace netdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Cambio de moneda de euro a sol\n");
+
+            Console.WriteLine("\nIngrese el monto y luego presione Enter.\n");
+
+            double monto = Console.Read();
+
+            Console.WriteLine("El monto en soles es: " + eurosASoles(monto));
+
+        }
+
+
+        public static double eurosASoles(double cantidad){
+
+            return cantidad*obtenerCambioEuroASol();
+
+        }
+
+        public static double obtenerCambioEuroASol(){
+
+            string url = "http://localhost:8000/currency-exchange/from/EUR/to/PEN";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+            return m.conversionMultiple;
+
+        }
+
+    }
+
+}
